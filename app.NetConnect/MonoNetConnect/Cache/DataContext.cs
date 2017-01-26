e5dc0f99559c9ebbb6c2a54589780d358ccd6b5a
Redanski <joachimgotzes@gmail.com>
2017-01-26 23:35:04 +0100

    Nachtrag StartUP und ImageCache

--- app.NetConnect/MonoNetConnect/Cache/DataContext.cs
+++ app.NetConnect/MonoNetConnect/Cache/DataContext.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 
 namespace MonoNetConnect.Cache
 {
+    [Serializable]
     public partial class DataContext
     {
         private Boolean _isInitialLoad { get; set; } = true;
--- app.NetConnect/NetConnect/ApplicationStartup.cs
+++ app.NetConnect/NetConnect/ApplicationStartup.cs
@@ -9,11 +9,53 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using MonoNetConnect.Cache;
 
 namespace NetConnect
 {
-    public class ApplicationStartup
+    public class ApplicationStartup : Application, Application.IActivityLifecycleCallbacks
     {
         int a = 1;
+        public ApplicationStartup()
+        {
+            DataContext data = DataContext.GetDataContext();
+            data.UpdateDataContext();
+            AppDomain domain = AppDomain.CurrentDomain;
+
+        }
+        void IActivityLifecycleCallbacks.OnActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActivityLifecycleCallbacks.OnActivityDestroyed(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActivityLifecycleCallbacks.OnActivityPaused(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActivityLifecycleCallbacks.OnActivityResumed(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActivityLifecycleCallbacks.OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActivityLifecycleCallbacks.OnActivityStarted(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IActivityLifecycleCallbacks.OnActivityStopped(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
